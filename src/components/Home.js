37798e645100bbef24c74b0e06ada50d1189eddf
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2024-05-01 16:57:42 -0700

    sorted upper portion of portfolio

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,21 +3,55 @@ import { motion } from 'framer-motion';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+
 
 const Home = (props) => {
   return (
     <div className='top-padding'>
-        <Container>
-            <Row>
-                <Col className='d-flex justify-content-center'>
-                    <h1 className='m-2'>Software Developer</h1>
+        <Container className='p-5'>
+        <motion.div initial={{ opacity: 0 }} animate={{ opacity:1 }} exit={{ opacity: 0}} transition={{duration:3}}>
+            <Row className='pb-5'>
+                <Col md>
+                    <h1 className='display-1'>Software Developer</h1>
+                    <p className='info-top-padding roboto-medium'>Team oriented Full Stack Software Developer with hands-on experience in multiple stacks including Python and MERN. </p>
+                    <div className='d-flex'>
+                    <p className='info-top-padding roboto-thin pt-2'>&#60; Focus on clean code and learning the newest technologies /&#62;</p>
+                    <p className='info-top-padding roboto-thin p-4 pt-2'>&#60; Bringing creative approaches to research, analysis, and decision making /&#62; </p>
+                    </div>
+                    
+
                 </Col>
-                <Col>
-                <img
-                    className="d-block w-100"
-                    src="logo512.png"
-                    alt="First slide"
-                    />
+                <Col md>
+                    <Image fluid roundedCircle
+                        className="responsive"
+                        src="portoflio photo.jpeg"
+                        alt="web developer"
+                        />
+                </Col>
+            </Row>
+        </motion.div>
+            <Row className='info-top-padding text-center'>
+                <Col md>
+                    <Image fluid
+                        className=""
+                        src="web-fun.png"
+                        alt="web fun photo"
+                        />
+                </Col>
+                <Col md>
+                    <Image fluid
+                        className=""
+                        src="python-flask-mysql-sample.png"
+                        alt="python stack photo"
+                        />
+                </Col>
+                <Col md>
+                    <Image fluid
+                        className=""
+                        src="The MERN Stack.jpg"
+                        alt="mern stack photo"
+                        />
                 </Col>
             </Row>
         </Container>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,19 +4,12 @@ import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Offcanvas from 'react-bootstrap/Offcanvas';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
 import { motion } from 'framer-motion';
 
 
+
 const NavBar = () => {
-    const [show, setShow] = useState(false);
-    const [show1, setShow1] = useState(false);
     const [show2, setShow2] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    const handleClose1 = () => setShow1(false);
-    const handleShow1 = () => setShow1(true);
     const handleClose2 = () => setShow2(false);
     const handleShow2 = () => setShow2(true);
     const [windowWidth, setWindowWidth] = useState(window.innerWidth)
@@ -34,16 +27,15 @@ const NavBar = () => {
 
     return (
         <>
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity:1 }} exit={{ opacity: 0}} transition={{duration:1}}>
-        <Navbar bg="light" variant='light' className='fixed-top'>
+        <motion.div initial={{ opacity: 0 }} animate={{ opacity:1 }} exit={{ opacity: 0}} transition={{duration:3}}>
+        <Navbar className='fixed-top' style={{height:'160px'}}>
             <Container>
-            <Navbar.Brand as={Link} to="/" style={{fontSize:'x-large'}}>Jaime Quezada Jr</Navbar.Brand>
-                    {windowWidth<640?(
+            <Navbar.Brand as={Link} to="/" style={{fontSize:'x-large'}} className='roboto-black'>Jaime Quezada Jr</Navbar.Brand>
                     <Nav className=" justify-contend-end">
                         <Nav.Link as={Link}><h1><i className="bi bi-list" onClick={handleShow2}></i></h1></Nav.Link>
                         <Offcanvas placement='end' show={show2} onHide={handleClose2}>
                             <Offcanvas.Header closeButton>
-                            <Offcanvas.Title>Info</Offcanvas.Title>
+                            <Offcanvas.Title></Offcanvas.Title>
                             </Offcanvas.Header>
                             <Offcanvas.Body>
                         <Nav className=" justify-contend-end">
@@ -54,14 +46,6 @@ const NavBar = () => {
                         </Offcanvas.Body>
                         </Offcanvas>
                     </Nav>
-                    ):(
-                    <Nav className=" justify-contend-end">
-                        <Nav.Link style={{fontSize:'medium'}} as={Link} to="/about" className='d-flex align-items-center'>About</Nav.Link>
-                        <Nav.Link style={{fontSize:'medium'}}  as={Link} to="/projects"className='d-flex align-items-center'>Projects</Nav.Link>
-                        <Nav.Link style={{fontSize:'medium'}} as={Link} to="/contact" className='d-flex align-items-center'>Contact</Nav.Link>
-                    </Nav>
-                    )}
-                    
             </Container>
         </Navbar>
         </motion.div>
@@ -70,3 +54,31 @@ const NavBar = () => {
 }
 
 export default NavBar;
+
+{/* <Container fluid>
+<Navbar.Brand as={Link} to="/" style={{fontSize:'x-large'}}>Jaime Quezada Jr</Navbar.Brand>
+        {windowWidth<640?(
+        <Nav className=" justify-contend-end">
+            <Nav.Link as={Link}><h1><i className="bi bi-list" onClick={handleShow2}></i></h1></Nav.Link>
+            <Offcanvas placement='end' show={show2} onHide={handleClose2}>
+                <Offcanvas.Header closeButton>
+                <Offcanvas.Title>Info</Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+            <Nav className=" justify-contend-end">
+                <motion.div whileHover={{scale:0.9}} whileTap={{ scale: 1.1 }}><Nav.Link style={{fontSize:'large'}} as={Link} to="/about" show={show2} onClick={handleClose2}>About</Nav.Link></motion.div>
+                <motion.div whileHover={{scale:0.9}} whileTap={{ scale: 1.1 }}><Nav.Link style={{fontSize:'large'}} as={Link} to="/projects" show={show2} onClick={handleClose2}>Projects</Nav.Link></motion.div>
+                <motion.div whileHover={{scale:0.9}} whileTap={{ scale: 1.1 }}><Nav.Link style={{fontSize:'large'}} as={Link} to="/contact" show={show2} onClick={handleClose2}>Contact</Nav.Link></motion.div>
+                </Nav>
+            </Offcanvas.Body>
+            </Offcanvas>
+        </Nav>
+        ):(
+        <Nav className=" justify-contend-end">
+            <Nav.Link style={{fontSize:'medium'}} as={Link} to="/about" className='d-flex align-items-center'>About</Nav.Link>
+            <Nav.Link style={{fontSize:'medium'}}  as={Link} to="/projects"className='d-flex align-items-center'>Projects</Nav.Link>
+            <Nav.Link style={{fontSize:'medium'}} as={Link} to="/contact" className='d-flex align-items-center'>Contact</Nav.Link>
+        </Nav>
+        )}
+        
+</Container> */}
