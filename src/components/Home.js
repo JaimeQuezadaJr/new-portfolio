388db1ce1bf518f43bcad314d614c1dae4a2f751
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2024-05-03 14:10:11 -0700

    added projects section

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,28 +16,28 @@ const Home = (props) => {
                 <Col md>
                 <h1 className='display-1'>Software</h1>
                 <TypeAnimation
-                className='display-1'
-                sequence={[
-                    'Developer', // Types 'One'
-                    2000, // Waits 1s
-                    '', // Deletes 'One' and types 'Two'
-                    2000, // Waits 2s
-                    'Developer', // Types 'Three' without deleting 'Two'
-                    () => {
-                    console.log('Sequence completed');
-                    },
-                ]}
-                wrapper="span"
-                cursor={true}
-                repeat={Infinity}
-                style={{ display: 'inline-block' }}
+                    className='display-1'
+                    sequence={[
+                        'Developer', // Types 'One'
+                        2000, // Waits 1s
+                        '', // Deletes 'One' and types 'Two'
+                        2000, // Waits 2s
+                        'Developer', // Types 'Three' without deleting 'Two'
+                        () => {
+                        console.log('Sequence completed');
+                        },
+                    ]}
+                    wrapper="span"
+                    cursor={true}
+                    repeat={Infinity}
+                    style={{ display: 'inline-block' }}
             />
                     <p className='info-top-padding roboto-medium'>Team oriented Full Stack Software Developer with hands-on experience in multiple stacks including Python and MERN. </p>
                     <div className='d-flex'>
                         <p className='info-top-padding roboto-thin pt-2'>&#60; Focus on clean code and learning the newest technologies /&#62;</p>
                         <p className='info-top-padding roboto-thin p-4 pt-2'>&#60; Bringing creative approaches to research, analysis, and decision making /&#62; </p>
                     </div>
-                    <motion.button whileHover={{scale: 1.6}} whileTap={{ scale: 0.9 }} type="button" className="btn btn-light roboto-light shadow">About Me</motion.button>
+                    <motion.button whileHover={{scale: 1.6}} whileTap={{ scale: 0.9 }} type="button" className="btn btn-light roboto-light shadow mb-5">About Me</motion.button>
                 </Col>
                 <Col md>
                     <i class="bi bi-code-slash"></i>
@@ -55,9 +55,27 @@ const Home = (props) => {
 
             <Row className='info-top-padding text-center pt-5 pb-5 mt-5 mb-5'>
                 <Col>
-                <h1 className='display-5 roboto-thin'>Developer Skills</h1>
+                <h1 className='display-5 roboto-thin'>Developer</h1>
+                <TypeAnimation
+                    className='display-5 roboto-thin'
+                    sequence={[
+                        'Skillset', // Types 'One'
+                        2000, // Waits 1s
+                        '', // Deletes 'One' and types 'Two'
+                        2000, // Waits 2s
+                        'Skillset', // Types 'Three' without deleting 'Two'
+                        () => {
+                        console.log('Sequence completed');
+                        },
+                    ]}
+                    wrapper="span"
+                    cursor={true}
+                    repeat={Infinity}
+                    style={{ display: 'inline-block' }}
+            />
+                
                 <p className='info-top-padding roboto-thin pt-2'>&#60; Proven experience using web fundamentals as well as full stack python and MERN development. /&#62;</p>
-                <motion.button whileHover={{scale:1.6}} whileTap={{ scale: 0.9 }} type="button" className="btn btn-light roboto-light shadow m-3">My Work</motion.button>
+                <motion.button whileHover={{scale:1.6}} whileTap={{ scale: 0.9 }} type="button" className="btn btn-light roboto-light shadow m-3">My Resume</motion.button>
                 </Col>
                 <Col md className='d-flex align-items-center'>
                     <motion.div whileHover={{scale:0.9}} whileTap={{ scale: 1.1 }}>
@@ -89,6 +107,55 @@ const Home = (props) => {
             </Row>
 
             <hr></hr>
+            <Row className='mb-5 pb-5 mt-5 pt-5'>
+                <Col lg={8}>
+                    <video className='img-fluid video-corners' autoPlay muted loop>
+                        <source src="nutrilogvideo720.mp4" type="video/mp4"/>
+                    </video>
+                </Col>
+                <Col lg={4}>
+                    <h1 className='roboto-regular mb-5'>nutrilog</h1>
+                    <motion.div whileHover={{scale:0.9}} whileTap={{ scale: 1.1 }}><p className='roboto-regular'>&#60; MongoDB / Express.js / Node.js / React.js /&#62;</p></motion.div>
+                    <motion.div whileHover={{scale:0.9}} whileTap={{ scale: 1.1 }}><p className='roboto-regular'>&#60; Created RESTful API in order to allow user CRUD operations /&#62;</p></motion.div>
+                    <motion.div whileHover={{scale:0.9}} whileTap={{ scale: 1.1 }}><p className='roboto-regular'>&#60; Integrated the USDA FoodData Central API to incorporate nutrition data /&#62;</p></motion.div>
+                    <motion.button whileHover={{scale: 1.1}} whileTap={{ scale: 0.9 }} type="button" className="btn btn-light roboto-light shadow mb-5">Learn More</motion.button>
+                </Col>
+            </Row>
+            <Row className='pt-5 mt-5 pb-5 mb-5'>
+                <Col lg={4}>
+                    <h1 className='roboto-regular mb-5'>TravelMem</h1>
+                    <motion.div whileHover={{scale:0.9}} whileTap={{ scale: 1.1 }}><p className='roboto-regular'>&#60; Python / Flask / MySQL /&#62;</p></motion.div>
+                    <motion.div whileHover={{scale:0.9}} whileTap={{ scale: 1.1 }}><p className='roboto-regular'>&#60; Used back end validation to verify user input and keep data consistent for user CRUD operations /&#62;</p></motion.div>
+                    <motion.div whileHover={{scale:0.9}} whileTap={{ scale: 1.1 }}><p className='roboto-regular'>&#60; Utilized one-to-many and many-to-many relationships within MySQL to connect all registered users, allowing for liking posts and adding comments /&#62;</p></motion.div>
+                    <motion.button whileHover={{scale: 1.1}} whileTap={{ scale: 0.9 }} type="button" className="btn btn-light roboto-light shadow mb-5">Learn More</motion.button>
+                </Col>
+                <Col lg={8}>
+                    <video className='img-fluid video-corners' autoPlay muted loop>
+                        <source src="TravelMem New Copy.mp4" type="video/mp4"/>
+                    </video>
+                </Col>
+            </Row>
+            <hr></hr>
+                <Row className='mb-5 mt-5 pt-5'>
+                    <Col>
+                        <h1 className='text-center roboto-light'>Design</h1>
+                        <p className='info-top-padding roboto-thin pt-2 text-center'>&#60; Focus on clean code and learning the newest technologies /&#62;</p>
+                    </Col>
+                    <Col md className='d-flex align-items-center'>
+                        <motion.div whileHover={{scale:0.9}} whileTap={{ scale: 1.1 }}>
+                            <Image fluid
+                                className=""
+                                src="Web-Developer-Header.png"
+                                alt="web developer photo"
+                                />
+                        </motion.div>
+                    </Col>
+                    <Col>
+                        <h1 className='text-center roboto-light'>Development</h1>
+                        <p className='info-top-padding roboto-thin pt-2 text-center'>&#60; Focus on clean code and learning the newest technologies /&#62;</p>
+                    </Col>
+                </Row>
+            <hr></hr>
 
             <Row className='d-flex justify-content-center mb-5 mt-5 pt-5'>
                 <Col className='d-flex justify-content-center' >
@@ -115,10 +182,12 @@ const Home = (props) => {
                 </Col>
             </Row>
             </motion.div>
+
+            
         </Container>
         <footer className='bg-light p-2'>
             <div className='d-flex justify-content-around'>
-                <p className='m-0 roboto-light'>Designed & Developed by Jaime Quezada Jr</p>
+                {/* <p className='m-0 roboto-light'>Designed & Developed by Jaime Quezada Jr</p> */}
                 <p className='m-0 roboto-light'>Copyright &#169; Jaime Quezada Jr 2024</p>
             </div>
                 
